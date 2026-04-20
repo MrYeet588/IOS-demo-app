5dc5ff39eea48cd344f93f1143aa0b47f3461180
BT_C09_06 <mobileappdev@bths.edu>
2026-04-20 13:25:42 -0400

    Initial Commit

---
+++ Gitty/GittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittyApp.swift
+//  Gitty
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
