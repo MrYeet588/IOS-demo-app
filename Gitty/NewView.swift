816f5a5c79b28c1cc856c1c62883901c246fb84f
BT_C09_06 <mobileappdev@bths.edu>
2026-04-20 13:33:07 -0400

    eff

---
+++ Gitty/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  Gitty
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+struct NewView: View {
+    var body: some View {
+        VStack {
+            Text("sample, gitty!")
+            Text("sample, gitty!")
+
+        }
+        .padding()
+    }
+}
